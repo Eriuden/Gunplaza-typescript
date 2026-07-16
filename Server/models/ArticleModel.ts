efb3f0d6470df8a1fc7b80a239ada9c80a82510d
unknown <antoinecarrette@hotmail.fr>
2026-07-16 08:21:03 +0200

    build: article model 16/07/2026

---
+++ Server/models/ArticleModel.ts
@@ -0,0 +1,59 @@
+import { Schema, Document, model } from "mongoose"
+const mongoose = require("mongoose")
+
+export interface IArticle extends Document  {
+    picture: string,
+    name: string,
+    grade: string,
+    price: string,
+    comments : [
+        commenterId: string,
+        commenterName : string,
+        text: string,
+        timestamp: Number
+  ],
+    likers: [string],
+    dislikers: [string]
+}
+
+const articleSchema= new Schema(
+    {
+        picture: {            
+            required: true 
+        },
+
+        name: {            
+            trim: true,
+            maxlength: 200,
+            required: true,
+        },
+
+        grade: {            
+            trim: true,
+            maxlength: 20,
+            required: true,
+        },
+
+        price: {           
+            trim: true,
+            max: 10,
+            required: true,
+        },
+
+        comments: {
+            required: true,
+        },
+
+        likers: {            
+          required: true,
+        },
+
+        dislikers: {
+            required: true,
+        }
+    },
+    {timestamps : true}
+)
+
+const articleModel = model<IArticle>("article", articleSchema)
+module.exports = articleModel
