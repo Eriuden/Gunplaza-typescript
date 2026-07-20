404b3e501824b244d59ac5b719be2e656550d6cc
unknown <antoinecarrette@hotmail.fr>
2026-07-20 09:34:13 +0200

    build: cart page 20/07/2026

---
+++ Client/src/pages/Cart.tsx
@@ -0,0 +1,27 @@
+import { useSelector} from "react-redux";
+
+export const Cart = () => {
+    const cartData = useSelector((state: any)=> state.cartReducer)
+
+    return (
+      <div>
+          <div>
+              {cartData.map((article: any)=> {
+                  <>
+                      <div>
+                          <h2>{article.name}</h2>
+                          <img src={article.picture}/>
+                          <h3>{article.grade}</h3>
+                          <h3>{article.price}</h3>
+                      </div>
+                  </>           
+              })}
+          </div>
+  
+          <div>
+              <span>{cartData.totalPrice}</span>
+          </div>
+          
+      </div>
+    )
+}
